de4e66d59fc013bd2cc44edf8cd6a0c10876f12b
0Kareem0 <kimo123kimo321@gmail.com>
2025-04-09 15:23:47 +0200

    changed the port for no reason :D

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8383
 
 const logger = require('./middleware/logger')
 const peopleRoutes = require('./routes/peopleRoutes')
